c67fc7025f80759a78c76ee28e5694814f8dd767
bhakti.palkar <bhakti.palkar@macs-g.com>
2024-05-02 21:03:31 +0530

    update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -24,7 +24,7 @@ const register = (username, email, password) => {
 };
 
 const login = (email, password) => {
-  console.log(process.env.REACT_APP_API_URL)
+  console.log(API_URL)
   return axios
     .post(API_URL + "login", {
       email,
